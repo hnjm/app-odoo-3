00ab4fef1f358809a58a37d0b8c9c0a6e8f7b7a2
Hassan Najm <hassannjm@gmail.com>
2022-10-15 07:36:35 +0300

    Initial commit

---
+++ app_web_enterprise/static/src/js/list_renderer.js
@@ -0,0 +1,13 @@
+odoo.define('app_web_enterprise.ListRenderer', function (require) {
+    "use strict";
+
+    var ListRenderer = require('web.ListRenderer');
+
+    ListRenderer.include({
+        // _renderEmptyRow: function () {
+        //     return;
+        // },
+    });
+
+    return ListRenderer;
+});
---
+++ l10n_cn_standard_latest/models/res_currency.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, exceptions, _
+
+
+class ResCurrency(models.Model):
+    _inherit = 'res.currency'
